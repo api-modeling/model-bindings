07d53c35206ecf0e72c610bf4d30bc52f776cf19
mattdfuchs <mfuchs@mfuchs-ltm4.internal.salesforce.com>
2021-01-05 16:29:39 -0800

    added a test case for two raml files, trying to get parser to work

--- src/test/bindings/APIContractBindingsPlugin.test.ts
+++ src/test/bindings/APIContractBindingsPlugin.test.ts
@@ -14,6 +14,25 @@ import {
     ApiModelDialect
 } from "@api-modeling/api-modeling-metadata"
 import {ApiParser} from "../../main/bindings/utils/apiParser";
+import exp from "constants";
+import { DocumentResourceLoader } from '@api-modeling/api-modeling-metadata'
+import * as amf from '@api-modeling/amf-client-js';
+//import { client } from '@api-modeling/amf-client-js'
+
+export class AResourceLoader extends DocumentResourceLoader {
+    fetch(resource: string): Promise<amf.client.remote.Content> {
+      //const textUrl =  '../../' + resource.substring(7) //model-bindings/src/test/resources/example.raml'
+      const textUrl =  //'src/main/resources/test/' +
+          (resource.startsWith('http://goop.com/') ? resource.substring('http://goop.com/'.length) : resource)
+      const textData = fs.readFileSync(textUrl).toString();
+      const longer = 'file:///Users/mfuchs/Documents/webspace/api-mod-grp/auto-store/metadata-store/'+textUrl
+      const fetched = new amf.client.remote.Content(textData, resource)
+      return Promise.resolve(fetched)
+    }
+    accepts(resource: string): boolean {
+      return true;
+    }
+  }
 
 describe('APIBindingsPlugin', function() {
     this.timeout(5000);
@@ -82,12 +101,15 @@ describe('APIBindingsPlugin', function() {
 
     it('should parse RAML Library specs and generate matching modules', async function () {
         const apiPlugin = new APIContractBindingsPlugin();
-        const textUrl = "src/test/resources/example.raml"
+        const textUrl = "http://goop.com/src/test/resources/apiMulti/api.raml"
+        //"src/test/resources/example.raml"
         //"src/test/resources/library.raml";
-        const textData = fs.readFileSync(textUrl).toString();
+        //const textData = fs.readFileSync(textUrl).toString();
+        const loader = new AResourceLoader()
         const parsed = await apiPlugin.import(
-            [{name: "format", value: ApiParser.RAML1}, {name: "syntax", value: ApiParser.YAML}],
-            [{ url: "file://"+ textUrl, text: textData}]
+            [{name: "format", value: ApiParser.RAML1}, {name: "syntax", value: ApiParser.YAML},{name:"loader", value: loader}],
+            [{ url: textUrl, text: <string><unknown>null}]
+//            [{ url: "file://"+ textUrl, text: textData}]
         );
         assert.equal(parsed.length, 4); // all the models: modules, entities, bindings
 
@@ -101,7 +123,8 @@ describe('APIBindingsPlugin', function() {
 
         assert.equal(allModules, 2)
         assert.equal(dataModels.length, 2)
-        assert.equal(allEntities, 8)
+        assert.equal(allEntities, 3)
+        //assert.equal(allEntities, 8)
         assert.equal(allBindings, dataModels.length)
     });
 
